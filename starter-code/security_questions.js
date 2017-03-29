768b5414596f984edf5bc46cd691c0d258802cbe
janeosaur <janewie@gmail.com>
2017-03-29 08:53:51 -0700

    added security questions

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,15 @@
 console.log("security_questions.js is loaded");
+
+var securityQuestions = [
+  { question: "how old are you?", expectedAnswer: "22" },
+  { question: "where are you from?", expectedAnswer: "VA" },
+  { question: "do you like SF?", expectedAnswer: "yes" }
+]
+
+for (var i=0; i < securityQuestions.length; i++){
+  userAnswer = prompt(securityQuestions[i].question);
+  if (userAnswer != securityQuestions[i].expectedAnswer){
+      alert("Incorrect security question response!");
+      break;
+    }
+  }
